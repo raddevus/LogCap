2758710a4c28e1e7b218567d2dc7a078930b6033
raddevus <1radeutsch1@gmail.com>
2025-08-01 15:36:02 -0400

    Added basic WebInfo class to hold the ip address & associated info that will be logged

---
+++ Model/WebInfo.cs
@@ -0,0 +1,7 @@
+class WebInfo{
+   public Int64 ID{get; set;}
+   public String SiteDesc{get; set;} // allos.dev, cyapass.com, newlibre.com, etc.
+   public String IppAddr{get; set;}
+   public DateTime Created {get; set;}
+}
+
