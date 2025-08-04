a29f2335a839b8c04d6b27ac10b0acbc9596c73b
raddevus <1radeutsch1@gmail.com>
2025-08-04 09:17:29 -0400

    Added example of how to run so webapi is visible to local network (for testing).

--- runOnPort.sh
+++ runOnPort.sh
@@ -1,1 +1,5 @@
 dotnet run --urls="http://localhost:5899"
+# You can set ip address to 0.0.0.0 
+# and then the app will be available to local network
+# via ip address of device it is running on
+# dotnet run --urls="http://0.0.0.0:5899"
