8593ec27e90c9819de082fa11bc61baa297de550
raddevus <1radeutsch1@gmail.com>
2025-08-06 10:06:55 -0400

    Updated WebInfo class to a C# record, to see the C# record in action

--- Model/WebInfo.cs
+++ Model/WebInfo.cs
@@ -1,20 +1,15 @@
 namespace LogCap.Model;
-
-public class WebInfo{
-   public Int64 ID{get; set;}
-   public String SiteDesc{get; set;} // allos.dev, cyapass.com, newlibre.com, etc.
-   public String IpAddress{get; set;}
-   public String RefUrl{get; set;}
-   public String Info{get;set;}
-   public DateTime Created {get; set;}
-
-   public WebInfo(String siteDesc, String ipAddress, String refUrl=null, String info=null){
-      SiteDesc = siteDesc;
-      IpAddress = ipAddress;
-      RefUrl = refUrl;
-      Info = info;
-      Created = DateTime.Now;
-   }
-
+public record WebInfo(
+    long ID,
+    string SiteDesc,
+    string IpAddress,
+    string? RefUrl = null,
+    string? Info = null,
+    DateTime Created = default
+)
+{
+    public WebInfo(string siteDesc, string ipAddress, string? refUrl = null, string? info = null)
+        : this(0, siteDesc, ipAddress, refUrl, info, DateTime.Now)
+    {
+    }
 }
-
